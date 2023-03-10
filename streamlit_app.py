f5d744618d954ef7dd7dac9fc0a1b2715f5cbb3f
mdougramaji <127525184+mdougramaji@users.noreply.github.com>
2023-03-10 14:47:31 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,12 @@ streamlit.title('YHYH CMON BIG TINGS')
 
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Beer')
-streamlit.text('Pint')
-streamlit.text('Lager')
+streamlit.text('🥣 Beer')
+streamlit.text('🥣 Pint')
+streamlit.text('🥣 Lager')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
